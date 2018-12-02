af6569964d3b471e31d5cd74029f7817f601b7ed
cs16048 <arrowsuke@yahoo.co.jp>
2018-12-02 22:27:16 +0900

    プレイヤーの配置と移動処理

---
+++ Assets/F01_Script/C01_PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C01_PlayerController : MonoBehaviour {
+
+    private CharacterController charaController;//charaコンポーネント用変数
+    private Vector3 move = Vector3.zero;        //chara移動量ベクトル
+    private float speed = 5.0f;                 //スピード
+
+	// Use this for initialization
+	void Start () {
+        charaController = GetComponent<CharacterController>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        move = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        charaController.Move(move * speed * Time.deltaTime);    //chara移動
+	}
+}
