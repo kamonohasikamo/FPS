73294ff4067a178dd95be85f52b0b6fc0114cff4
cs16048 <arrowsuke@yahoo.co.jp>
2018-12-03 00:28:05 +0900

    プレイヤーの移動　修正

--- Assets/F01_Script/C01_PlayerController.cs
+++ Assets/F01_Script/C01_PlayerController.cs
@@ -23,13 +23,20 @@ public class C01_PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        playerCameraPosition_1stPerson();
+	}
+    
+    private void playerCameraPosition_1stPerson()
+    {
         float y = move.y;                                       //move.yの値を保持しておく。
-        move = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-        Vector3 playerDir = move;       //移動方向を取得
+        move = new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical"));
+        move = transform.TransformDirection(move);
         move *= speed;                                          //移動速度を乗算
+        
+        //ジャンプ&重力処理
         move.y += y;                                            //move.yの値を戻す
         if (charaController.isGrounded)                         //地面に足がついているかどうか
-        { 
+        {
             if (Input.GetKeyDown(KeyCode.Space))                    //Spaceが押されたら跳躍力分上にあげる
             {
                 move.y = jumpPower;
@@ -37,16 +44,44 @@ public class C01_PlayerController : MonoBehaviour {
         }
         move.y = move.y - (GRAVITY * Time.deltaTime);           //y軸方向に重力を追加(代入)
 
+        //playerの向き判定等
+        Vector3 playerDir = new Vector3(Input.GetAxis("Horizontal"), 0.0f,0.0f);
+        playerDir = transform.TransformDirection(playerDir);
         if (playerDir.magnitude > 0.1f)
         {
             Quaternion q = Quaternion.LookRotation(playerDir);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, q, rotationSpeed * Time.deltaTime);
         }
         charaController.Move(move * Time.deltaTime);            //chara移動
-	}
-    /*Updateの中身
-     *方程式で考えると
-     * 速度V = 初速度V0 + 重力加速度g * 時間t
-     * を計算している。
-     */
+    }
+
+    private void playerCameraPosition_3rdPerson()
+    {
+        //移動量
+        float y = move.y;
+        move = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        Vector3 playerDir = move;
+        move *= speed;                                          //移動速度を乗算
+
+        //ジャンプ&重力処理
+        move.y += y;                                            //move.yの値を戻す
+        if (charaController.isGrounded)                         //地面に足がついているかどうか
+        {
+            if (Input.GetKeyDown(KeyCode.Space))                    //Spaceが押されたら跳躍力分上にあげる
+            {
+                move.y = jumpPower;
+            }
+        }
+        move.y = move.y - (GRAVITY * Time.deltaTime);           //y軸方向に重力を追加(代入)
+
+        //playerの向き判定等
+        if (playerDir.magnitude > 0.1f)
+        {
+            Quaternion q = Quaternion.LookRotation(playerDir);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, q, rotationSpeed * Time.deltaTime);
+        }
+        charaController.Move(move * Time.deltaTime);            //chara移動
+    }
 }
+
+
---
+++ Assets/F01_Script/C02_CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C02_CameraController : MonoBehaviour {
+    private C01_PlayerController playerController;
+
+	// Use this for initialization
+	void Start () {
+        playerController = GameObject.FindWithTag("Player").GetComponent<C01_PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            changeCameraMove();
+        }
+	}
+
+    private void changeCameraMove()
+    {
+        if(transform.parent == null)
+        {
+            transform.parent        = playerController.transform;
+            transform.localPosition = Vector3.zero;
+        }
+        else
+        {
+            transform.parent        = null;
+            transform.position      = new Vector3(0, 4, -10);
+        }
+    }
+}
