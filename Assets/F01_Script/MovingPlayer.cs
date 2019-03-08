971027df513045796b4b906d2a6d068ca0479371
cs16048 <arrowsuke@yahoo.co.jp>
2019-03-08 16:01:30 +0900

    今度こそちゃんとジョイスティック実装できた(位置もいい感じ)

---
+++ Assets/F01_Script/MovingPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlayer : MonoBehaviour {
+	//先ほど作成したJoystick
+	[SerializeField]
+	private Joystick _joystick = null;
+
+	//移動速度
+	private const float SPEED = 0.1f;
+
+	private void Update () {
+	  Vector3 pos = transform.position;
+    pos.x += _joystick.Position.x * SPEED;
+    pos.z += _joystick.Position.y * SPEED;
+
+    transform.position = pos;
+  }
+}
