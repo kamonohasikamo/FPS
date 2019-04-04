de64c4a633585f94f8bac33e80b93dda01d95b82
kamonohasikamo <arrowsuke2211@yahoo.co.jp>
2019-04-04 19:15:50 +0900

    敵の追加と敵のモーションの追加（歩くモーション->打たれたら倒れる）

---
+++ Assets/F01_Script/C05_Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C05_Enemy : MonoBehaviour
+{
+	private Animator animator;		// Animator コンポーネント用変数
+
+
+	void Start() {
+		animator	=	GetComponent< Animator >();	// Animator コンポーネント取得
+	}
+
+	void damage() {
+		animator.SetBool("dead", true);	// Animator の変数 dead をtrueに変える処理
+	}
+}
